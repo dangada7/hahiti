53d8b13d28b388d93989b779f7da383320839966
vicban3d <vicban3d@gmail.com>
2015-12-30 12:39:53 +0200

    Create WebRequests.js

---
+++ WebRequests.js
@@ -0,0 +1,33 @@
+function createRequest() {
+    var result = null;
+    if (window.XMLHttpRequest) {
+        // FireFox, Safari, etc.
+        result = new XMLHttpRequest();
+        if (typeof result.overrideMimeType != 'undefined') {
+            result.overrideMimeType('text/xml'); // Or anything else
+        }
+    }
+    else if (window.ActiveXObject) {
+        // Internet Explorer
+        result = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else {
+        alert("Invalid Browser!");
+    }
+    return result;
+}
+
+function sendPOSTRequest(path, data){
+    var req = createRequest();
+    req.open("POST", Paths.ROOT + path, true);
+    req.setRequestHeader("Content-Type", "text/plain");
+    req.send(data);
+    return req;
+}
+
+function sendGETRequest(path, data){
+    var req = createRequest();
+    req.open("GET", Paths.ROOT + path, true);
+    req.setRequestHeader("Content-Type", "text/plain");
+    req.send(data);
+}

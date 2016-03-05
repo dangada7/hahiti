0473aea274a565a42af9e13414393d1e627382b4
dan gadasi <dangada2@gmail.com>
2016-03-05 12:45:32 +0200

    add user

--- js/rest.js
+++ js/rest.js
@@ -1,10 +1,9 @@
-
-
 var addUser = function(){
 	alert("add user");
 	$.ajax({
 		type: 'PUT',
 	   	url: 'http://requestb.in/1mt9rms1?inspect',
+	   	processData: false,
 	   	data: {
       		format: 'json',
       		Username: 'dan',
@@ -16,7 +15,31 @@ var addUser = function(){
    		dataType: 'jsonp',
 	   	success: function(data) {
 	   		console.log('success',data);
-	   	}
+	   	},
+	   	error: function (ajaxContext) {
+        	alert(ajaxContext.responseText)
+  		}
 	});
 };
 
+var addGroup = function(){
+	alert("add group");
+	$.ajax({
+		type: 'PUT',
+	   	url: 'http://requestb.in/1mt9rms1?inspect',
+	   	data: {
+      		format: 'json',
+      		Name: 'group1',
+      		Description: 'the best group everrrrr',
+      		ImageURL: 'Dan',
+      		Students: 's@s',
+   		},
+   		dataType: 'jsonp',
+	   	success: function(data) {
+	   		console.log('success',data);
+	   	},
+	   	error: function (ajaxContext) {
+        	alert(ajaxContext.responseText)
+  		}
+	});
+};

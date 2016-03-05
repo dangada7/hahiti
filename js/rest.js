c2d3d06ae8522b67c18b9e93ceea4fe18167580e
dan gadasi <dangada2@gmail.com>
2016-03-05 17:10:56 +0200

    add user

--- js/rest.js
+++ js/rest.js
@@ -4,8 +4,10 @@ var addUser = function(){
 
 	alert("add user");
 	$.ajax({
-	   	url: 'http://requestb.in/1n07sbo1',
-	   	method : "PUT",
+	   	//url: 'http://requestb.in/1n07sbo1',
+	   	url:'http://localhost:8080/hahiti/users/3',
+	   	method : "POST",
+	   	data : '{"Username":"dan","Password":"123","Name":"dan","Email":"d@d","Settings":"MaxLate=5/MaxMissing=3/StatisticsFrequency=3"}',
 	   	success: function(data) {
 	   		console.log('success',data);
 	   	},
@@ -16,18 +18,19 @@ var addUser = function(){
 };
 
 var getuser1 = function(){
-
 	alert("add user");
 	$.ajax({
-	   	//url: 'http://requestb.in/1n07sbo1',
-	   	url:'http://localhost:8080/hahiti/users/3',
+	   	url: 'http://requestb.in/1n07sbo1',
+	   	//url:'http://localhost:8080/hahiti/users/3',
 	   	method : "GET",
 		dataType: "jsonp", 
 	   	success: function(data) {
 	   		console.log('success',data);
+	   		alert(data);
 	   	},
 	   	error: function (ajaxContext) {
         	alert(ajaxContext.responseText)
   		}
 	});
 };
+

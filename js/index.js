d86ca6733027884e99b864784125069073d7f666
dan gadasi <dangada2@gmail.com>
2016-03-06 21:15:32 +0200

    home"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
 	
+	var userName = sessionStorage.getItem('username');
+	if(userName!=null){
+		$("#loginButton").hide();
+		$("#signinButton").hide();
+	}else{
+		$("#logoutButton").hide();
+	}
+	
+	$("#logoutButton").click( function(){
+		alert("logout");
+		sessionStorage.setItem('username',null);
+	});
+
+
+
+
 	var loginFunc = function(userName){
 	   	sessionStorage.setItem("username",userName);
 	   	window.location.replace("home.html");
